aea0d240d5481a29cf72d64480a811b212c21a7a
Chris Stook <chris@chrisstook.com>
2019-01-12 18:26:21 -0500

    sweep2

--- analyze_sweep.jl
+++ analyze_sweep.jl
@@ -3,9 +3,9 @@ using DataFrames
 using CSV
 using StatPlots
 
-df = CSV.read("sweep.csv",header=1)
+df = CSV.read("sweep2.csv",header=1)
 df.coil_area = df.coil_outer_diameter .* df.coil_length
-@df df scatter(:coil_area,:force)
+
 sort!(df, (:coil_area,:force))
 
 
@@ -22,6 +22,11 @@ let max_force = 0.0, i = 1
         i=i+1
     end
 end
-
 deleterows!(coil_vs_force,a)
-@df coil_vs_force scatter!(:coil_area,:force_maximum)
+
+@df df scatter(:coil_area,:force,xlabel="coil_area",ylabel="force")
+@df coil_vs_force plot!(:coil_area,:force_maximum)
+
+@df coil_vs_force scatter(:cap_length,:coil_length ./ :cap_length, xlabel="cap_length",ylabel="coil_length/cap_length")
+@df coil_vs_force scatter(:coil_outer_diameter,:coil_length ./ :cap_length, xlabel="coil_outer_diameter",ylabel="coil_length/cap_length")
+@df coil_vs_force scatter(:coil_area,:coil_length ./ :cap_length, xlabel="coil_area",ylabel="coil_length/cap_length")
