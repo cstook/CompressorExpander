bf70979d4c61a4d5dea35f3a848fef150c46bca7
Chris Stook <chris@chrisstook.com>
2019-01-15 12:58:26 -0500

    fix analyze_sweep for coil volume

--- analyze_sweep.jl
+++ analyze_sweep.jl
@@ -4,13 +4,14 @@ using CSV
 using StatPlots
 
 df = CSV.read("sweep2.csv",header=1)
-df.coil_area = df.coil_outer_diameter .* df.coil_length
-
-sort!(df, (:coil_area,:force))
+df.coil_volume = π.*df.coil_length.*(df.coil_outer_diameter.-df.tube_outer_diameter)
 
 
+sort!(df, (:coil_volume,:force))
 
-coil_vs_force = by(df, [:coil_area,:cap_length,:coil_outer_diameter,:coil_length], :force=>maximum)
+
+
+coil_vs_force = by(df, [:coil_volume,:cap_length,:coil_outer_diameter,:coil_length], :force=>maximum)
 a = Array{Int,1}()
 let max_force = 0.0, i = 1
     for row in eachrow(coil_vs_force)
@@ -24,9 +25,9 @@ let max_force = 0.0, i = 1
 end
 deleterows!(coil_vs_force,a)
 
-@df df scatter(:coil_area,:force,xlabel="coil_area",ylabel="force")
-@df coil_vs_force plot!(:coil_area,:force_maximum)
+@df df scatter(:coil_volume,:force,xlabel="coil_volume",ylabel="force")
+@df coil_vs_force plot!(:coil_volume,:force_maximum)
 
 @df coil_vs_force scatter(:cap_length,:coil_length ./ :cap_length, xlabel="cap_length",ylabel="coil_length/cap_length")
 @df coil_vs_force scatter(:coil_outer_diameter,:coil_length ./ :cap_length, xlabel="coil_outer_diameter",ylabel="coil_length/cap_length")
-@df coil_vs_force scatter(:coil_area,:coil_length ./ :cap_length, xlabel="coil_area",ylabel="coil_length/cap_length")
+@df coil_vs_force scatter(:coil_volume,:coil_length ./ :cap_length, xlabel="coil_volume",ylabel="coil_length/cap_length")
